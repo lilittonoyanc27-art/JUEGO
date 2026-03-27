29ac84674a4f1abf7aeaaed64181e734afa56ed2
lilittonoyanc27-art <lilit.tonoyanc27@gmail.com>
2026-03-27 20:53:42 +0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import GorGayaneGenderBattle from './GorGayaneGenderBattle';
+
+export default function App() {
+  return (
+    <div className="relative min-h-screen">
+      <GorGayaneGenderBattle />
+    </div>
+  );
+}
